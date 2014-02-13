8053af55e4f38eadc750846686eebdfc847b38fb
Malgorzata Jatczyk <guska076@gmail.com>
2014-02-13 21:15:34 +0100

    simple php server example

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$socket = stream_socket_server("tcp://0.0.0.0:8000", $errno, $errstr);
+if (!$socket) {
+  echo "$errstr ($errno)<br />\n";
+} else {
+  while ($conn = stream_socket_accept($socket)) {
+    while($command= fread($conn,1024)) {
+		if(trim($command) == 'volup') {
+			exec('amixer set Master 10%+');
+		}
+		if(trim($command) == 'voldown') {
+			exec('amixer set Master 10%-');
+		}
+		echo $command;
+    }
+    fclose($conn);
+  }
+  fclose($socket);
+}
+?>
